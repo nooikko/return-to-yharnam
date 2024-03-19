2a0bfab7a5462c5374510db02d72489f1cbc8d7f
Quinn Penney <elijahpenney@outlook.com>
2024-03-19 14:08:10 -0700

    Added twitter image and some more SEO props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,14 @@ export const metadata: Metadata = {
   description: 'A yearly event where players return to the game of Bloodborne to play together and help new players.',
   keywords:
     'bloodborne, bloodborne ps5, co-op, when is return to yharnam, online, multiplayer, return to yharnam, yharnam, 2024, fromsoftware, event',
+  twitter: {
+    images: './twitter.webp',
+    title: 'Return to Yharnam - 2024',
+  },
+  openGraph: {
+    images: './twitter.webp',
+    title: 'Return to Yharnam - 2024',
+  },
 };
 
 export default function RootLayout({
