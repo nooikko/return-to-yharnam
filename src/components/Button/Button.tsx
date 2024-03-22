e15f19f293aa9bd3544775bf7618f15aeed6544f
Quinn Penney <elijahpenney@outlook.com>
2024-03-22 14:26:48 -0700

    Added button hover effect

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,7 +8,10 @@ interface ButtonProps extends PropsWithChildren, PropsWithTailwind {
 
 export const Button: React.FC<ButtonProps> = ({ children, className, onClick }) => {
   return (
-    <button onClick={onClick} className={twMerge('rounded-md bg-red-800 px-8 py-3 text-sm font-bold uppercase text-white', className)}>
+    <button
+      onClick={onClick}
+      className={twMerge('rounded-md bg-red-800 px-8 py-3 text-sm font-bold uppercase text-white transition ease-in-out hover:scale-105', className)}
+    >
       {children}
     </button>
   );
